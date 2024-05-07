0c388d527a31c84d0a2ab8f931737a24dc35bf5c
KerchOfissial <154542610+KerchOfissial@users.noreply.github.com>
2024-05-07 18:46:52 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -2,6 +2,9 @@ import re
 
 inp = input("Введите строку с датой(-ами): ")
 
-dat = re.findall('\d\d.\d\d.\d\d', inp)
-
+dat = re.findall('\d{2}.\d{2}.\d{2}\s', inp)
+dat_2 = re.findall('\d{2}.\d{2}.\d{4}', inp)
+for i in dat_2:
+    dat.append(i)
+    
 print(dat)
