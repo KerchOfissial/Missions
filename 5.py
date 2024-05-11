44605d7ca332777e675550285be440d0078f3922
KerchOfissial <154542610+KerchOfissial@users.noreply.github.com>
2024-05-11 19:15:08 +0300

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,24 @@
+import re
+while True:
+    inp = input("Введите пароль: ")
+    r = re.search('.{8,24}', inp)
+    if r == None:
+        print(f'{inp} - ненадёжный пароль!')
+        continue
+    else:
+        r = re.search('[A-Z][a-z]', inp)
+        if r == None:
+            print(f'{inp} - ненадёжный пароль!')
+            continue
+        else:
+            r = re.search('\d', inp)
+            if r == None:
+                print(f'{inp} - ненадёжный пароль!')
+                continue
+            else:
+                r = re.search('[-_/?!@<>]', inp)
+                if r == None:
+                    print(f'{inp} - ненадёжный пароль!')
+                    continue
+                else:
+                    print("Пароль сохранён")
